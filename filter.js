58d29d22889ce40d7d879b32a05172c2157d6bc3
Durgesh Awasthi <awasthi.durgesh16@gmail.com>
2016-11-22 10:22:16 +0530

    Add files via upload

--- filter.js
+++ filter.js
@@ -1,121 +1,124 @@
 var fs=require('fs');
 var lineReader = require('readline').createInterface({
-   input : fs.createReadStream('js/csv/FoodFacts.csv') 
+ input : fs.createReadStream('js/csv/FoodFacts.csv','utf8') 
 });
+
+
 var i=0;
+
+var arrayCountry=['Netherlands', 'Canada', 'United Kingdom' , 'United States' , 'Australia' , 'France' , 'Germany' , 'Spain', 'South Africa'];
+var northEurope = ['United Kingdom', 'Denmark', 'Sweden','Norway'];
+var centralEurope= ['France', 'Belgium', 'Germany', 'Switzerland','Netherlands'];
+var southEurope = ['Portugal', 'Greece', 'Italy', 'Spain', 'Croatia','Albania'];
+var countryIndex=0,fatIndex=0,proteinIndex=0,carboIndex=0,saltIndex=0,sugarIndex=0;
+var northEuropeFatData =0,northEuropeProteinData =0,northEuropeCarboData =0,centralEuropeFatData =0,centralEuropeProteinData =0,centralEuropeCarboData =0;
+var southEuropeFatData =0,southEuropeProteinData =0,southEuropeCarboData =0;
 var lineRecords=[];
-var Header = 0;
-var countryIndex=0,fatIndex=0,protienIndex=0,carboIndex=0,saltIndex=0,sugarIndex=0;
-var countryArray = ['Netherlands', 'Canada', 'United Kingdom' , 'United States' , 'Australia' , 'France' , 'Germany' , 'Spain', 'South Africa'];
-var sugarArray = [] ,saltArray = [], jsonArray = [],count=[];
-var north = ['United Kingdom', 'Denmark', 'Sweden','Norway'];
-var central  = ['France', 'Belgium', 'Germany', 'Switzerland','Netherlands'];
-var South = ['Portugal', 'Greece', 'Italy', 'Spain', 'Croatia','Albania'];
-var nfatData =0,nprotienData=0,ncarboData =0,cfatData =0,cprotienData =0,ccarboData =0, sfatData =0,sprotienData=0,scarboData=0;
-var jsonArray = [];
-var Europe=[];
-var nobj={},cobj={},sobj={};
-var sCounter=0,cCounter=0,nCounter=0;
-while(i<countryArray.length)
-  // initialise array
+var arraySugar= [];
+var arraySalt = [];
+
+
+
+for(var i=0;i<arrayCountry.length;i++)  // initialise all array to 0
 {
-  sugarArray[i]=0;
-  saltArray[i]=0;
-  // count[i]=0;
+  arraySugar[i]=0;
+  arraySalt[i]=0;
+  
 }
 
+var isHeader=0;
 lineReader.on('line',function(line)
 {
-    lineRecords = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/); 
-      // split data  line by line 
-
-    if(Header==0)         // it is for fetching index of coloumn
+    lineRecords = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);   // split data  line by line using regular expression 
+    if(isHeader==0)           // For fetching index of coloumn
     {
-        countryIndex = lineRecords.indexOf("countries_en");
-        protienIndex = lineRecords.indexOf("proteins_100g");
-        carboIndex = lineRecords.indexOf("carbohydrates_100g");
-        fatIndex = lineRecords.indexOf("fat_100g"); 
-        saltIndex = lineRecords.indexOf("salt_100g");
-        sugarIndex = lineRecords.indexOf("sugars_100g");
-        // console.log(countryIndex);
-      Header++;
-    
-    }   
+      countryIndex = lineRecords.indexOf("countries_en");
+      proteinIndex = lineRecords.indexOf("proteins_100g");
+      carboIndex = lineRecords.indexOf("carbohydrates_100g");
+      fatIndex = lineRecords.indexOf("fat_100g"); 
+      saltIndex = lineRecords.indexOf("salt_100g");
+      sugarIndex = lineRecords.indexOf("sugars_100g");
+      isHeader++;
+      
+    }
+
     else
     {
-        if( countryArray.includes(lineRecords[countryIndex]))    //it is for fetching all data
-         {
-            var index = countryArray.indexOf(lineRecords[countryIndex]);
-            var salt = lineRecords[saltIndex].replace("",0);
-            var sugar=lineRecords[sugarIndex].replace("",0);
-            saltArray[index] = saltArray[index]+parseFloat(salt);
-            sugarArray[index] = sugarArray[index]+parseFloat(sugar);
-            count[index]+=1;
-            
-         }
-        if(north.includes(lineRecords[countryIndex])) // it is for checking north countries
-         {
-        var nfat = lineRecords[fatIndex].replace("",0);
-        nprotien=lineRecords[protienIndex].replace("",0);
-        ncarbo=lineRecords[carboIndex].replace("",0);
-        nfatData+=parseFloat(nfat);
-        ncarboData+=parseFloat(ncarbo);
-        nprotienData+=parseFloat(nprotien);
-        nCounter++;
-         }
-      if(central.includes(lineRecords[countryIndex]))  // it is for checking central countries
-         {
-        var cfat = lineRecords[fatIndex].replace("",0);
-        var cprotien=lineRecords[protienIndex].replace("",0);
-        var ccarbo=lineRecords[carboIndex].replace("",0);
-        cfatData+=parseFloat(cfat);
-        cprotienData+=parseFloat(cprotien);
-        ccarboData+=parseFloat(ccarbo);
-        cCounter++;
-         }
-      if(South.includes(lineRecords[countryIndex]))   // it is for checking south countries
-         {
-        var sfat = lineRecords[fatIndex].replace("",0);
-        var sprotien=lineRecords[protienIndex].replace("",0);
-        var scarbo=lineRecords[carboIndex].replace("",0);
-        sfatData+=parseFloat(sfat);
-        sprotienData+=parseFloat(sprotien);
-        scarboData+=parseFloat(scarbo);
-        sCounter++;  
-         }
-       }
- });
- 
-lineReader.on('close',function ()       //put data into object
+        if( arrayCountry.includes(lineRecords[countryIndex]))    // Fetching all data
+        {
+          var index = arrayCountry.indexOf(lineRecords[countryIndex]);
+          var salt = lineRecords[saltIndex].replace("",0);
+          var sugar=lineRecords[sugarIndex].replace("",0);
+          arraySalt[index] = arraySalt[index]+parseFloat(salt);
+          arraySugar[index] = arraySugar[index]+parseFloat(sugar);
+          
+        }
+
+
+        if(northEurope.includes(lineRecords[countryIndex])) // For North Europian countries
+        {
+          var northEuropeFat = lineRecords[fatIndex].replace("",0);
+          var northEuropeProtein=lineRecords[proteinIndex].replace("",0);
+          var northEuropeCarbo=lineRecords[carboIndex].replace("",0);
+          northEuropeFatData+=parseFloat(northEuropeFat);
+          northEuropeCarboData+=parseFloat(northEuropeCarbo);
+          northEuropeProteinData+=parseFloat(northEuropeProtein);
+          
+        }
+        if(centralEurope.includes(lineRecords[countryIndex]))  // For Central Europe countries
+        {
+          var centralEuropeFat = lineRecords[fatIndex].replace("",0);
+          var centralEuropeProtein=lineRecords[proteinIndex].replace("",0);
+          var centralEuropeCarbo=lineRecords[carboIndex].replace("",0);
+          centralEuropeFatData+=parseFloat(centralEuropeFat);
+          centralEuropeProteinData+=parseFloat(centralEuropeProtein);
+          centralEuropeCarboData+=parseFloat(centralEuropeCarbo);
+          
+        }
+        if(southEurope.includes(lineRecords[countryIndex]))   // For South Europe countries
+        {
+          var southEuropeFat = lineRecords[fatIndex].replace("",0);
+          var southEuropeProtein=lineRecords[proteinIndex].replace("",0);
+          var southEuropeCarbo=lineRecords[carboIndex].replace("",0);
+          southEuropeFatData+=parseFloat(southEuropeFat);
+          southEuropeProteinData+=parseFloat(southEuropeProtein);
+          southEuropeCarboData+=parseFloat(southEuropeCarbo);
+          
+        }
+      }
+    });
+
+var arrayJson= []; 
+lineReader.on('close',function ()       
 
 { 
-     for(var j=0;j<countryArray.length;j++)  //put data into object
+  for(var j=0;j<arrayCountry.length;j++)  //Put data into object
   {
     var obj = {};
-    obj["country"] = countryArray[j];
-    obj["salt"] = saltArray[j];
-    obj["sugar"] = sugarArray[j];
-    jsonArray.push(obj);
+    obj["country"] = arrayCountry[j];
+    obj["salt"] = arraySalt[j];
+    obj["sugar"] = arraySugar[j];
+    arrayJson.push(obj);
   }
-  fs.writeFile('json/Countries1.json', JSON.stringify(jsonArray) , 'utf-8');
-    nobj["regions"] = "NorthEurope";
-    nobj["Fat"] = nfatData/nCounter;
-    nobj["Protien"] = nprotienData/nCounter;
-    nobj["carbohydrates"] = ncarboData/nCounter;
-    Europe.push(nobj);
-    cobj["regions"] = "CentralEurope";
-    cobj["Fat"] = cfatData/cCounter;
-    cobj["Protien"] = cprotienData/cCounter;
-    cobj["carbohydrates"] = ccarboData/cCounter;
-    Europe.push(cobj);
-    sobj["regions"] = "SouthEurope";
-    sobj["Fat"] = sfatData/sCounter;
-    sobj["Protien"] = sprotienData/sCounter;
-    sobj["carbohydrates"] = scarboData/sCounter;
-    Europe.push(sobj);
-    fs.writeFile('json/Countries2.json',JSON.stringify(Europe),'utf-8');
-    console.log(jsonArray);
-    console.log(Europe);
-
-});
-
+  var Europe=[];
+  var northEuropeObj={},centralEuropeObj={},southEuropeObj={};
+   fs.writeFile('json/countries1.json', JSON.stringify(arrayJson) , 'utf-8');   // Writing to file
+   northEuropeObj["regions"] = "northEurope";
+   northEuropeObj["Fat"] = northEuropeFatData;
+   northEuropeObj["Protien"] = northEuropeProteinData;
+   northEuropeObj["carbohydrates"] = northEuropeCarboData;
+   Europe.push(northEuropeObj);
+   centralEuropeObj["regions"] = "centralEurope";
+   centralEuropeObj["Fat"] = centralEuropeFatData;
+   centralEuropeObj["Protien"] = centralEuropeProteinData;
+   centralEuropeObj["carbohydrates"] = centralEuropeCarboData;
+   Europe.push(centralEuropeObj);
+   southEuropeObj["regions"] = "southEurope";
+   southEuropeObj["Fat"] = southEuropeFatData;
+   southEuropeObj["Protien"] = southEuropeProteinData;
+   southEuropeObj["carbohydrates"] = southEuropeCarboData;
+   Europe.push(southEuropeObj);
+   fs.writeFile('json/countries2.json',JSON.stringify(Europe),'utf-8');
+   console.log(arrayJson);
+   console.log(Europe);
+ });
